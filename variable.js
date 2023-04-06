587a3675733d97d4888f20df4acf74fc005d12a2
Liamcr <cariou.liam@orange.fr>
2023-04-06 11:06:19 +0200

    exo 3

---
+++ variable.js
@@ -0,0 +1,6 @@
+var myNumber = 10;
+var myString = "Hello World";
+var myBoolean = true;
+console.log("myNumber =", myNumber);
+console.log("myString =", myString);
+console.log("myBoolean =", myBoolean);
---
+++ variable.ts
@@ -0,0 +1,7 @@
+let myNumber: number = 10;
+let myString: string = "Hello World";
+let myBoolean: boolean = true;
+
+console.log("myNumber =", myNumber);
+console.log("myString =", myString);
+console.log("myBoolean =", myBoolean);
